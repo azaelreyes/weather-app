93d669f3dfd936b70529c4b0648bf4beb0f94c01
Azael Reyes-Morales <azaelreyes-morales@Azaels-MacBook-Pro.local>
2023-06-14 08:07:23 -0700

    fixed issue with forecast not showing when api doesnt return 7 days

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const forecastWind = document.querySelectorAll('.forecast-day-wind')
 const forecastDayImg = document.querySelectorAll('.forecast-day-img')
 
 async function getWeather(city){
-    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=73ab6e334e1f4e74bf1225335230105&q=${city}&days=7&aqi=no&alerts=no`);
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=73ab6e334e1f4e74bf1225335230105&q=${city}&days=10&aqi=no&alerts=no`);
     const weather = await response.json();
 
     //Everything Below Here is filling out the HTML
@@ -50,26 +50,41 @@ async function getWeather(city){
     sunrise.innerHTML=`${weather.forecast.forecastday[0].astro.sunrise.toLocaleString('en-US', options).replace(/^0/, '')}`
     sunset.innerHTML=`${weather.forecast.forecastday[0].astro.sunset.toLocaleString('en-US', options).replace(/^0/, '')}`
     tempImg.src = `./weather/day/${weather.current.condition.icon.split('/').pop()}`
-    
+
     forecastWeekday.forEach((day, i)=>{
-        const d = new Date(weather.forecast.forecastday[i+1].date);
-        d.setDate(d.getDate() + 1);
-        day.innerHTML = d.toLocaleString('en-US', {weekday: 'long'});
+        if(weather.forecast.forecastday[i+1]){
+            const d = new Date(weather.forecast.forecastday[i+1].date);
+            d.setDate(d.getDate() + 1);
+            day.innerHTML = d.toLocaleString('en-US', {weekday: 'long'});
+        }
+        
     });
     forecastDayTemp.forEach((temp,i)=>{
-        temp.innerHTML = `${Math.round(weather.forecast.forecastday[i+1].day.maxtemp_f)} °F`
+        if(weather.forecast.forecastday[i+1]){
+            temp.innerHTML = `${Math.round(weather.forecast.forecastday[i+1].day.maxtemp_f)} °F`
+        }
+        
     })
     forcastNightTemp.forEach((temp,i)=>{
-        temp.innerHTML = `${Math.round(weather.forecast.forecastday[i+1].day.mintemp_f)} °F`
+        if(weather.forecast.forecastday[i+1]){
+            temp.innerHTML = `${Math.round(weather.forecast.forecastday[i+1].day.mintemp_f)} °F`
+        }
     })
     tableChanceOfRain.forEach((per,i)=>{
-        per.innerHTML = `${weather.forecast.forecastday[i+1].day.daily_chance_of_rain}%`
+        if(weather.forecast.forecastday[i+1]){
+            per.innerHTML = `${weather.forecast.forecastday[i+1].day.daily_chance_of_rain}%`
+        }
     })
     forecastWind.forEach((wind, i)=>{
-        wind.innerHTML = `${Math.round(weather.forecast.forecastday[i+1].day.maxwind_mph)} mph`
+        if(weather.forecast.forecastday[i+1]){
+            wind.innerHTML = `${Math.round(weather.forecast.forecastday[i+1].day.maxwind_mph)} mph`
+        }
     })
     forecastDayImg.forEach((icon,i)=>{
-        icon.src=`./weather/day/${weather.forecast.forecastday[i+1].day.condition.icon.split('/').pop()}`
+        if(weather.forecast.forecastday[i+1]){
+            icon.src=`./weather/day/${weather.forecast.forecastday[i+1].day.condition.icon.split('/').pop()}`
+        }
+        
     })
 
     loadingScreen.style.visibility = 'hidden';
